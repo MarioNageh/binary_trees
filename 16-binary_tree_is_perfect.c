22f904e3f251e961a4de72cb153a55cd4374d778
Mario Nageh <marionageh7@gmail.com>
2023-08-31 02:57:52 +0300

    Tree Is Perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -26,17 +26,20 @@ int get_tree_height(const binary_tree_t *tree)
 int is_perfect(const binary_tree_t *tree, int max_height, int level)
 {
 	if (!tree)
-        return (max_height == level);
+		return (max_height == level);
 
 	if ((tree->left && (!tree->right)) || (tree->right && !tree->left))
 		return (0);
-	
+
+
 	if (!tree->left && !tree->right)
 		return (max_height == level + 1);
+
 
 	return (is_perfect(tree->left, max_height, level + 1) &&
 			is_perfect(tree->right, max_height, level + 1));
 }
+
 
 
 /**
@@ -47,6 +50,9 @@ int is_perfect(const binary_tree_t *tree, int max_height, int level)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int max_height = 0, level = 0;
+
+	if (!tree)
+		return (0);
 
 	max_height = get_tree_height(tree);
 	return (is_perfect(tree, max_height, level));
