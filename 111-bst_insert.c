2e5369c25514ab95e70f0f72e89ebf690a8f74ed
Mario Nageh <marionageh7@gmail.com>
2023-08-31 17:43:16 +0300

    BST INSERT

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,54 +1,50 @@
 #include "binary_trees.h"
 #include <limits.h>
 
-
-bst_t *find_value_place(bst_t *tree, int value)
-{
-	
-	if (tree->n > value)
-	{
-		if(tree->left)
-			return (find_value_place(tree->left, value));
-		else
-			return (tree);
-	}
-	else if (tree->n < value)
-	{
-		if(tree->right)
-			return (find_value_place(tree->right, value));
-		else
-			return (tree);
-	}
-	else
-		return (NULL);
-}
-
+/**
+ * bst_insert - inserts a value in a Binary Search Tree
+ * @tree: double pointer to the root node of the BST to insert the value
+ * @value: value to store in the node to be inserted
+ * Return: pointer to the created node, or NULL on failure
+*/
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *new_node,*place;
+	bst_t *new_node, *current;
 
 	new_node = binary_tree_node(NULL, value);
-	if (!new_node)
+	if (new_node == NULL)
 		return (NULL);
 
-	if (!tree || !(*tree))
+	if (*tree == NULL)
 	{
 		*tree = new_node;
 		return (new_node);
 	}
-
-	place = find_value_place(*tree, value);
-	if (!place)
+	current = *tree;
+	while (current != NULL)
 	{
-		free(new_node);
-		return (NULL);
+		if (value < current->n)
+		{
+			if (current->left == NULL)
+			{
+				current->left = new_node;
+				new_node->parent = current;
+				return (new_node);
+			}
+			current = current->left;
+		}
+		else if (value > current->n)
+		{
+			if (current->right == NULL)
+			{
+				current->right = new_node;
+				new_node->parent = current;
+				return (new_node);
+			}
+			current = current->right;
+		}
+		else
+			return (NULL);
 	}
-
-	if (place->n > value)
-		place->left = new_node;
-	else
-		place->right = new_node;
-
-	new_node->parent = place;
 	return (new_node);
 }
