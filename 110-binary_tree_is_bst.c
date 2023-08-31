70224e83658b74f9731acf18daa918cc33d1376f
Mario Nageh <marionageh7@gmail.com>
2023-08-31 11:40:40 +0300

    Binary Search Tree

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+int is_subtree_less_than(const binary_tree_t *tree, int value) {
+    if (!tree) {
+        return true;
+    }
+
+    return (tree->n < value) &&
+           is_subtree_less_than(tree->left, value) &&
+           is_subtree_less_than(tree->right, value);
+}
+
+int is_subtree_greater_than(const binary_tree_t *tree, int value) {
+    if (!tree) {
+        return true;
+    }
+
+    return (tree->n > value) &&
+           is_subtree_greater_than(tree->left, value) &&
+           is_subtree_greater_than(tree->right, value);
+}
+
+int binary_tree_is_bst(const binary_tree_t *tree) {
+    if (!tree) {
+        return true;
+    }
+
+    int left_bst = is_subtree_less_than(tree->left, tree->n) && binary_tree_is_bst(tree->left);
+    int right_bst = is_subtree_greater_than(tree->right, tree->n) && binary_tree_is_bst(tree->right);
+
+    return left_bst && right_bst;
+}
