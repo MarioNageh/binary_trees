3eec1d0ff0e4364bce72b3d51305a6afdf907733
Mario Nageh <marionageh7@gmail.com>
2023-09-01 03:32:55 +0300

    Fix2 Inorder Succ

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -29,74 +29,73 @@ bst_t *swap(bst_t *a, bst_t *b)
 
 }
 
-bst_t *bst_remove(bst_t *root, int value)
-{
-	bst_t *current, *successor;
+bst_t *bst_remove(bst_t *root, int value) {
+    if (!root)
+        return NULL;
 
-	if (!root)
-		return (NULL);
-	current = root;
+    bst_t *current = root;
+    bst_t *parent = NULL; // To keep track of parent node
+    while (current && current->n != value) {
+        parent = current; // Update parent before moving to the next node
+        if (value < current->n)
+            current = current->left;
+        else
+            current = current->right;
+    }
 
-	while (current && current->n != value)
-	{
-		if (value < current->n)
-			current = current->left;
-		else
-			current = current->right;
-	}
+    if (!current)
+        return root;
 
-	if (!current)
-		return (NULL);
-	
-	/*Cas If No Children*/
-	if (!current->left && !current->right)
-	{
-		if (current->parent)
-		{
-			if (current->parent->left == current)
-				current->parent->left = NULL;
-			else
-				current->parent->right = NULL;
-		}
-		else
-			root = NULL;
-		free(current);
-		return (root);
-	}
+    // Case: No Children
+    if (!current->left && !current->right) {
+        if (parent) {
+            if (parent->left == current)
+                parent->left = NULL;
+            else
+                parent->right = NULL;
+        } else {
+            root = NULL;
+        }
+        free(current);
+        return root;
+    }
 
+    // Case: One Child
+    if (!current->right) {
+        if (parent) {
+            if (parent->left == current)
+                parent->left = current->left;
+            else
+                parent->right = current->left;
+        } else {
+            root = current->left;
+        }
+        free(current);
+        return root;
+    }
+    if (!current->left) {
+        if (parent) {
+            if (parent->left == current)
+                parent->left = current->right;
+            else
+                parent->right = current->right;
+        } else {
+            root = current->right;
+        }
+        free(current);
+        return root;
+    }
 
-	if (!current)
-		return (NULL);
-	/*Cas If On Children*/
-	if (!current->right)
-	{
-		swap(current, current->left);
-		free(current);
-		return (root);
-	}
-	if (!current->left)
-	{
-		swap(current, current->right);
-		free(current);
-		return (root);
-	}
+    // Case: Two Children
+    bst_t *successor = find_inorder_successor(current);
+    current->n = successor->n;
+    
+    // Remove the successor node from the tree
+    if (successor->parent->left == successor)
+        successor->parent->left = NULL;
+    else
+        successor->parent->right = NULL;
+    free(successor);
 
-	if (current->left && current->right)
-	{
-		successor = find_inorder_successor(current);
-		printf("Successor: %d\n", successor->n);
-		printf("Current: %d\n", current->n);
-		current->n = successor->n;
-		if (successor->parent->left == successor)
-			successor->parent->left = NULL;
-		else
-			successor->parent->right = NULL;
-		free(successor);
-
-
-
-
-		return (root);
-	}
-	return (NULL);
+    return root;
 }
